2654115ec2bbdb55b466276bd81a47cdfb51344a
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2026-04-26 18:55:06 -0500

    Mensajes para solicitar información al usuario creados

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ let tipoDocumento = "";
 let numeroDocumento = "";
 let salario, pension, comisiones, horasExtra, nivelRiesgo;
 
+nombreCompleto = prompt("Escriba su nombre");
+tipoDocumento = prompt("Ingrese su Tipo de Documento");
+numeroDocumento = prompt("Escriba su Número de Documento");
+edad = parseInt(prompt("Ingrese su edad"));
+
 
 // Validar Edad
 
@@ -32,8 +37,13 @@ else if (edad >= 18 && edad < 25) {
   alert("Usuario beneficiario por cotizante. No se calcularán las obligaciones.");
 }
 else if (edad >= 25 && edad < 60) {
-  // Calcular 
+  // Cálculo de obligaciones leborales
+  salario = prompt("Ingrese su salario (en COP)");
+  comisiones = prompt("Ingrese el valor de sus comisiones, si aplica (en COP)") 
+  horasExtra = prompt("Ingrese el valor de sus hotas extra, si aplica (en COP)")
+  horasExtra = prompt("Ingrese su nivel de riesgo") 
 }
+
 else if (edad > 60) {
   // Pensionado
 }
