f541e5dbc5f55369f6464109889b0824541fe6f8
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2026-04-26 19:13:40 -0500

    Cálculos de salario agregados

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ Simulación de Obligaciones Laborales
 */
 
 
-//Constantes de Cálculo
+// Constantes de Cálculo
 
 const $SMMLV = 1750905;
 const $SUB_TRANS = 249095; 
 const $UVT = 52.37;
 
 
-//Variables para pedir al usuario
+// Variables para pedir al usuario
 
 let nombreCompleto = "";
 let edad = 0;
 let tipoDocumento = "";
 let numeroDocumento = "";
-let salario, pension, comisiones, horasExtra, nivelRiesgo;
+let salario, mesadaPensional, comisiones, horasExtra, nivelRiesgo;
+let salud, pension, auxilioTrans;
 
 nombreCompleto = prompt("Escriba su nombre");
 tipoDocumento = prompt("Ingrese su Tipo de Documento");
@@ -28,20 +29,37 @@ numeroDocumento = prompt("Escriba su Número de Documento");
 edad = parseInt(prompt("Ingrese su edad"));
 
 
+// Funciones
+
+function calcularPorcentaje(base, porcentaje){
+  return base * porcentaje;
+}
+
+
 // Validar Edad
 
 if (edad < 18) {
   alert("No es posible continuar: El usuario es menor de edad.");
-  }
+}
 else if (edad >= 18 && edad < 25) {
   alert("Usuario beneficiario por cotizante. No se calcularán las obligaciones.");
 }
 else if (edad >= 25 && edad < 60) {
-  // Cálculo de obligaciones leborales
+
+  // Cálculo de obligaciones leborales ---------------------------------------------
+
   salario = prompt("Ingrese su salario (en COP)");
-  comisiones = prompt("Ingrese el valor de sus comisiones, si aplica (en COP)") 
-  horasExtra = prompt("Ingrese el valor de sus hotas extra, si aplica (en COP)")
-  horasExtra = prompt("Ingrese su nivel de riesgo") 
+  comisiones = prompt("Ingrese el valor de sus comisiones, si aplica (en COP)"); 
+  horasExtra = prompt("Ingrese el valor de sus hotas extra, si aplica (en COP)");
+  nivelRiesgo = prompt("Ingrese su nivel de riesgo"); 
+
+  let totalDevengado = salario + comisiones + horasExtra;
+
+  let ibc = calcularPorcentaje(totalDevengado, 0.7);
+  salud = calcularPorcentaje(ibc, 0.04);
+  fondoSolidaridad = ibc >= $SMMLV * 4 ? calcularPorcentaje(ibc, 0.01) : 0;
+  pension = calcularPorcentaje(ibc, 0.04) + fondoSolidaridad;
+  auxilioTrans = salario <= $SMMLV * 2 ? $SUB_TRANS : 0;
 }
 
 else if (edad > 60) {
